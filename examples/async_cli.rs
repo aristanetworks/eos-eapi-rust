afa573c66d0821082365558659002c544da9c4e6
Vlad Hanciuta <vlad@arista.com>
2022-08-29 13:49:58 -0400

    Fix copyright notice

--- examples/async_cli.rs
+++ examples/async_cli.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-2016, Arista Networks, Inc.
+// Copyright (c) 2022, Arista Networks, Inc.
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
